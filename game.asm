87b3cb631f42e37f789ede05bfc884f7fdf44cb0
lunarec <jkjk98765@naver.com>
2022-03-24 15:26:52 -0400

    first commit

---
+++ game.asm
@@ -0,0 +1,43 @@
+#####################################################################
+#
+# CSCB58 Winter 2022 Assembly Final Project
+# University of Toronto, Scarborough
+#
+# Student: Minseok(Joshua) Kim, 1007462684, kimmi264, minseokk.kim@mail.utoronto.ca
+#
+# Bitmap Display Configuration:
+# - Unit width in pixels: 8 (update this as needed)
+# - Unit height in pixels: 8 (update this as needed)
+# - Display width in pixels: 256 (update this as needed)
+# - Display height in pixels: 512 (update this as needed)
+# - Base Address for Display: 0x10008000 ($gp)
+#
+# Which milestones have been reached in this submission?
+# (See the assignment handout for descriptions of the milestones)
+# - Milestone 1/2/3 (choose the one the applies)
+#
+# Which approved features have been implemented for milestone 3?
+# (See the assignment handout for the list of additional features)
+# 1. (fill in the feature, if any)
+# 2. (fill in the feature, if any)
+# 3. (fill in the feature, if any)
+# ... (add more if necessary)
+#
+# Link to video demonstration for final submission:
+# - (insert YouTube / MyMedia / other URL here). Make sure we can view it!
+#
+# Are you OK with us sharing the video with people outside course staff?
+# - yes / no / yes, and please share this project github link as well!
+#
+# Any additional information that the TA needs to know:
+# - (write here, if any)
+#
+#####################################################################
+######## Constants
+.eqv 	SCREEN_WIDTH	32	# units
+.eqv 	SCREEN_HEIGHT	64	# units
+.eqv    FRAME_DELAY     20
+
+# gameplay settings
+.eqv    JUMP_SEED   10
+.eqv    MOVE_SPEED  2
