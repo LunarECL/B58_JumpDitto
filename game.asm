f1d0b007d2d582f8f9cfa1e6450adcab0a7a88b0
lunarec <jkjk98765@naver.com>
2022-03-26 01:19:26 -0400

    Updated

--- game.asm
+++ game.asm
@@ -6,8 +6,8 @@
 # Student: Minseok(Joshua) Kim, 1007462684, kimmi264, minseokk.kim@mail.utoronto.ca
 #
 # Bitmap Display Configuration:
-# - Unit width in pixels: 8 (update this as needed)
-# - Unit height in pixels: 8 (update this as needed)
+# - Unit width in pixels: 4 (update this as needed)
+# - Unit height in pixels: 4 (update this as needed)
 # - Display width in pixels: 256 (update this as needed)
 # - Display height in pixels: 512 (update this as needed)
 # - Base Address for Display: 0x10008000 ($gp)
@@ -33,11 +33,35 @@
 # - (write here, if any)
 #
 #####################################################################
+.eqv    FRAME_BUFFER    0x10008000
+.eqv 	INPUT_BUFFER	0xffff0000
+
 ######## Constants
-.eqv 	SCREEN_WIDTH	32	# units
-.eqv 	SCREEN_HEIGHT	64	# units
+.eqv 	SCREEN_WIDTH	64	# units
+.eqv 	SCREEN_HEIGHT	128	# units
 .eqv    FRAME_DELAY     20
 
 # gameplay settings
 .eqv    JUMP_SEED   10
 .eqv    MOVE_SPEED  2
+
+# colours
+.eqv    DITTO_COLOUR_1  0xD8A3E0
+.eqv    DITTO_COLOUR_2  0xEDD0F2
+
+.text
+.macro pop_stack (%reg)
+	lw %reg, 0($sp)
+	addi $sp, $sp, 4
+.end_macro
+
+.macro push_stack (%reg)
+	addi $sp, $sp, -4 
+	sw %reg, 0($sp)
+.end_macro
+
+.globl main
+
+main:
+
+
